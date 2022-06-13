ea58ff92fce239cbffa9ec021fab470103523345
Elias Ferreira <71013006+elias31072002@users.noreply.github.com>
2022-06-13 09:50:17 -0300

    alterando singleton

--- Engenharia III/Singleton/singletonpattern/Singleton.java
+++ Engenharia III/Singleton/singletonpattern/Singleton.java
@@ -2,11 +2,11 @@ package observerpattern
 
 class Singleton {
 
-	// ReferÍncia de vari·vel est·tica de single_instance
+	// Refer√™ncia de vari√°vel est√°tica de single_instance
 	// do tipo Singleton
     private static Singleton single_instance = null;
  
-    // Declarando uma vari·vel do tipo String
+    // Declarando uma vari√°vel do tipo String
     public String s;
  
     // Construtor
@@ -14,11 +14,11 @@ class Singleton {
     // restrito a esta classe em si
     private Singleton()
     {
-        s = "Hello I am a string part of Singleton class";
+        s = "Ol√° eu sou um texto que faz parte para a classe singleton";
     }
  
-    // MÈtodo est·tico
-    // MÈtodo est·tico para criar inst‚ncia da classe Singleton
+    // M√©todo est√°tico
+    // M√©todo est√°tico para criar inst√¢ncia da classe Singleton
     public static Singleton getInstance()
     {
         if (single_instance == null)
@@ -32,43 +32,43 @@ class Singleton {
 //Classe principal
 class GFG {
 
-	// MÈtodo do driver principal
+	// M√©todo do driver principal
     public static void main(String args[])
     {
 
-    	// Instanciando a classe Singleton com a vari·vel x
+    	// Instanciando a classe Singleton com a vari√°vel x
         Singleton x = Singleton.getInstance();
  
 
-        // Instanciando a classe Singleton com a vari·vel y
+        // Instanciando a classe Singleton com a vari√°vel y
         Singleton y = Singleton.getInstance();
  
 
-        // Instanciando a classe Singleton com a vari·vel z
+        // Instanciando a classe Singleton com a vari√°vel z
         Singleton z = Singleton.getInstance();
  
-        // Imprimindo o cÛdigo hash para a vari·vel acima como
+        // Imprimindo o c√≥digo hash para a vari√°vel acima como
         // declarado
-        System.out.println("Hashcode de x È "
+        System.out.println("Hashcode de x √© "
                            + x.hashCode());
-        System.out.println("Hashcode de y È "
+        System.out.println("Hashcode de y √© "
                            + y.hashCode());
-        System.out.println("Hashcode de z È "
+        System.out.println("Hashcode de z √© "
                            + z.hashCode());
  
 
-        // VerificaÁ„o de condiÁ„o
+        // Verifica√ß√£o de condi√ß√£o
         if (x == y && y == z) {
  
-        	// Imprime declaraÁ„o
+        	// Imprime declara√ß√£o
             System.out.println(
-                "TrÍs objetos apontam para o mesmo local de memÛria no heap, ou seja, para o mesmo objeto");
+                "Tr√™s objetos apontam para o mesmo local de mem√≥ria no heap, ou seja, para o mesmo objeto");
         }
  
         else {
-        	// Imprime declaraÁ„o
+        	// Imprime declara√ß√£o
             System.out.println(
-                "TrÍs objetos N√O apontam para o mesmo local de memÛria no heap");
+                "Tr√™s objetos N√ÉO apontam para o mesmo local de mem√≥ria no heap");
         }
     }
 }
